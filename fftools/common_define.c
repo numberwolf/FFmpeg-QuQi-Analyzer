492f0f86b2a480d874e8883b3b2fd2e9af6fe893
小老虎 <numberwolf@xiaolaohudeMacBook-Pro.local>
2020-02-29 01:39:40 +0800

    dream car: PorscheGt3RS!!

---
+++ fftools/common_define.c
@@ -0,0 +1,15 @@
+//
+// Created by 小老虎 on 2020/2/28.
+//
+#include "common_define.h"
+
+void cyl_fftools_print_author_info(void) {
+    printf("- \n");
+    printf("- GitHub:https://github.com/numberwolf/ffmpeg-cyl-analyzer\n");
+    printf("- cyl-analyzer\n");
+    printf("- 此版本由 小老虎(Numberwolf/CYL)开发发起开发\n");
+    printf("- 邮箱：porschegt23@foxmail.com\n");
+    printf("- 提供FFmpeg获取视频的 Macro Block/QP/Motion Vector/...等编码分析信息的外显接口\n");
+    printf("- \n");
+}
+
---
+++ fftools/common_define.h
@@ -0,0 +1,14 @@
+//
+// Created by 小老虎 on 2020/2/28.
+// changyanlong
+//
+
+#ifndef CYL_FFMPEG_N4_1_3_COMMON_DEFINE_H
+#define CYL_FFMPEG_N4_1_3_COMMON_DEFINE_H
+#include <stdio.h>
+
+#define CYL_FFMPEG_INTRO_STR_ZH "此版本由 小老虎(Numberwolf/CYL[porschegt23@foxmail.com])开发发起开发; 可以在cyl-analyzer 获取视频的 Macro Block/QP/Motion Vector/...等编码分析信息"
+
+void cyl_fftools_print_author_info(void);
+
+#endif //CYL_FFMPEG_N4_1_3_COMMON_DEFINE_H
